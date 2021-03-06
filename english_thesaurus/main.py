e4dd29a5761d9a7d08e54a85b5041c9bc4a5d269
falsami <fahimalsafi@gmail.com>
2021-03-05 21:34:43 -0500

    first_app_done

--- english_thesaurus/main.py
+++ english_thesaurus/main.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,9 +7,23 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn =input("Did you mean %s instead? Enter Y if yes, or N if No. "% get_close_matches(w, data.keys())[0])
+        if yn == "Y" or  yn == "y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N" or  yn == "n":
+            return "The word doesn't exist"
+        else:
+            return "Wrong input"
     else:
         return "this word doesn't exist"
 
 word = input("Enter Word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
